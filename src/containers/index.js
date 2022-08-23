6aa3f424b95713ac8eac4d5ecc2529bb4b915c36
stikitobgxd <104159251+stikitobgxd@users.noreply.github.com>
2022-08-23 03:02:32 +0300

    new fileeees
    tre

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Features} from './features/Features';
-export { default as Footer} from './footer/Footer';
-export { default as Info} from './info/Info';
-export { default as Whatcrawenos} from './whatcrawenos/Whatcrawenos';
